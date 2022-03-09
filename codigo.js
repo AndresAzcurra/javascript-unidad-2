3946fa5064c722ff5e24e7dd5414198c8f205c3d
andres <andresazcurra@gmail.com>
2022-03-09 15:11:59 -0300

    trabajo practico de la unidad 2 javascript

---
+++ codigo.js
@@ -0,0 +1,22 @@
+var cantidadDeVentanasEnMiCasa = Number(4)
+var soyHumano = true 
+var miGustoDePizzaPreferido = "Fugazzetta"
+var multivar = true
+
+
+console.log ( "Mi gusto de pizza favorito es   "+ miGustoDePizzaPreferido )
+console.log (cantidadDeVentanasEnMiCasa)
+console.log (soyHumano)
+document.write (cantidadDeVentanasEnMiCasa)
+console.log (multivar)
+
+var multivar= "hola"
+console.log (multivar);
+
+
+
+var multivar = "minion";
+console.log (multivar)
+
+var multivar =0
+console.log (multivar)
